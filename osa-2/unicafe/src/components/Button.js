39005cc06d8e34c39f4e648e23e6efa779d4f4bb
Niko Salminen <niko.salminen@nitro.fi>
2018-05-30 23:06:40 +0300

    Turhat kopsitut rojut edellisestä osasta pois

--- osa-2/unicafe/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({text, vote, onClick}) => {
-    return (
-        <button onClick={() => onClick(vote)}>{text}</button>
-    );
-};
-
-export default Button;
